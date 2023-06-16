8e532d236f991e38c3e9e6c9ee785f02be5d8972
Boris Bošković <boris.boskovic92@gmail.com>
2023-06-16 15:40:44 +0200

    Initial commit

---
+++ TicketMachine/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Packaging;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Markup;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace TicketMachine
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PrintDialog printDialog;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            code_input_box.Focus();
+            printDialog = new PrintDialog();
+        }
+
+        private void code_input_box_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                do_print_stuff(code_input_box.Text.Trim());
+                code_input_box.Text = "";
+            }
+        }
+
+        /// <summary>For printing stuff</summary>
+        /// <exception cref="System.Printing.PrintQueueException">Exception is thrown if no printer with the specified name is found</exception>
+        private void do_print_stuff(string text)
+        {
+            string printerName = "POS-80";
+            var printQueue = new LocalPrintServer().GetPrintQueue(printerName);
+
+            PrintCapabilities printCapabilities = printQueue.GetPrintCapabilities();
+
+            PageContent pageContent = new PageContent();
+            pageContent.Width = printCapabilities.OrientedPageMediaWidth ?? 0;
+            pageContent.Height = printCapabilities.OrientedPageMediaHeight ?? 0;
+
+            Canvas canvas = new Canvas();
+            canvas.Width = pageContent.Width;
+            canvas.Height = pageContent.Height;
+
+            var textBlock = new TextBlock();
+            textBlock.Text = text;
+            canvas.Children.Add(textBlock);
+
+            var rect = new Rectangle();
+            rect.Stroke = new SolidColorBrush(Colors.Black);
+            rect.Width = canvas.Width;
+            rect.Height = 20;
+            canvas.Children.Add(rect);
+
+            FixedPage fixedPage = new FixedPage();
+            fixedPage.Children.Add(canvas);
+
+            FixedDocument fixedDocument = new FixedDocument();
+
+            ((IAddChild)pageContent).AddChild(fixedPage);
+
+            fixedDocument.Pages.Add(pageContent);
+
+            var documentWriter = PrintQueue.CreateXpsDocumentWriter(printQueue);
+            documentWriter.Write(fixedDocument);
+        }
+    }
+}
---
+++ TicketMachine/Printer/DocumentPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Printing;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace TicketMachine.Printer
+{
+    public class DocumentPrinter
+    {
+        PrintDialog printDialog = new PrintDialog();
+    }
+}
